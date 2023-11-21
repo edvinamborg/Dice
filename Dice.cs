1c5da260f1a59a396d1db142b8510caacb5dcd72
Edvin Amborg <edvin.amborg@gmail.com>
2023-11-21 10:31:44 +0100

    Add project files.

---
+++ Dice.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Security.Principal;
+
+namespace PictureShapes
+{
+    class Dice
+    {
+        
+        private Circle dot1;
+        private Circle dot2;
+        private Circle dot3;
+        private Circle dot4;
+        private Circle dot5;
+        private Circle dot6;
+        private Circle dot0;
+        private int number;
+        private static Random numberGenerator;
+        
+        
+
+
+        public Dice()
+        {
+            numberGenerator = new Random();
+            DrawImage();
+        }
+
+        public void Roll()
+        {
+            number = numberGenerator.Next(0, 7);
+
+            if (number % 2 != 0)
+            {
+                dot0.MakeVisible();
+            }
+            if (number >= 2)
+            {
+                dot1.MakeVisible();
+                dot2.MakeVisible();
+            }
+            if (number >= 4)
+            {
+                dot3.MakeVisible();
+                dot4.MakeVisible();
+            }
+            if (number == 6)
+            {
+                dot5.MakeVisible();
+                dot6.MakeVisible();
+            }
+
+
+        }
+        public void DrawImage()
+        {
+            dot1 = new Circle();
+            dot1.MakeInVisible();
+            dot1.ChangeColor("red");
+            dot1.ChangeSize(100);
+
+            dot2 = new Circle();
+            dot2.MakeInVisible();
+            dot2.ChangeColor("red");
+            dot2.ChangeSize(100);
+            dot2.MoveVertical(350);
+            dot2.MoveHorizontal(350);
+
+            dot3 = new Circle();
+            dot3.MakeInVisible();
+            dot3.ChangeColor("red");
+            dot3.ChangeSize(100);
+            dot3.MoveVertical(350);
+
+
+            dot4 = new Circle();
+            dot4.MakeInVisible();
+            dot4.ChangeColor("red");
+            dot4.ChangeSize(100);
+            dot4.MoveHorizontal(350);
+
+            dot5 = new Circle();
+            dot5.MakeInVisible();
+            dot5.ChangeColor("red");
+            dot5.ChangeSize(100);
+            dot5.MoveVertical(175);
+
+            dot6 = new Circle();
+            dot6.MakeInVisible();
+            dot6.ChangeColor("red");
+            dot6.ChangeSize(100);
+            dot6.MoveVertical(175);
+            dot6.MoveHorizontal(350);
+
+            dot0 = new Circle();
+            dot0.MakeInVisible();
+            dot0.ChangeColor("red");
+            dot0.ChangeSize(100);
+            dot0.MoveVertical(175);
+            dot0.MoveHorizontal(175);
+
+
+
+
+        }
+    }
+}
---
+++ PictureForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PictureShapes
+{
+    partial class PictureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.canvas = new PictureShapes.Canvas();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDraw.Location = new System.Drawing.Point(449, 618);
+            this.btnDraw.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(149, 46);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Roll";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(4, 4);
+            this.canvas.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1063, 607);
+            this.canvas.TabIndex = 1;
+            this.canvas.Click += new System.EventHandler(this.canvas_Click);
+            // 
+            // PictureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 663);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.btnDraw);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PictureForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private Canvas canvas;
+    }
+}
+
